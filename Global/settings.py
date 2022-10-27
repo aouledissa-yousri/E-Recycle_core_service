3a0d229e90d05c42e7f74f37720731f7a8f272a0
aouledissa <aouledissayousri@gmail.com>
2022-10-27 20:36:03 +0100

    finall version ready for deployment

--- Global/settings.py
+++ Global/settings.py
@@ -143,12 +143,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#EMAIL_HOST_USER = "aouledissayousri@gmail.com"
 EMAIL_HOST_USER = "e.recycle.mail.service@gmail.com"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#EMAIL_HOST_PASSWORD = "mdsvndstlxjffbfg"
 EMAIL_HOST_PASSWORD = "tpfqtwbylmvzrvxz"
 
 
